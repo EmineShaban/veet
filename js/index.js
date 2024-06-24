031001fc4d02060f723d5a68266310e3160ec720
Emine Shaban <emineshaban@icloud.com>
2024-06-24 09:50:45 +0300

    .

--- js/index.js
+++ js/index.js
@@ -17,3 +17,25 @@ less.addEventListener("click", function (e) {
     less.style.display = "none";
 
 })
+
+
+
+let faq = document.getElementsByClassName("faq-content");
+
+const onClick = (e) => {
+  if (
+    e.target.className == "q" &&
+    e.target.parentElement.children[1].style.display == "none"
+  ) {
+    e.target.parentElement.children[1].style.display = "block";
+  } else if (
+    e.target.parentElement.children[1].style.display == "block" &&
+    e.target.parentElement.children[1].className == "a"
+  ) {
+    e.target.parentElement.children[1].style.display = "none";
+  }
+};
+
+for (let index = 0; index < faq.length; index++) {
+  faq[index].addEventListener("click", onClick);
+}
