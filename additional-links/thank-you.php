17b8d5a8c3408a94576f705e70b0c6d28bf742fe
Emine Shaban <emineshaban@icloud.com>
2024-07-01 14:16:55 +0300

    .

--- additional-links/thank-you.php
+++ additional-links/thank-you.php
@@ -4,57 +4,76 @@
 <head>
     <meta charset="UTF-8">
     <title>Thank you for your order</title>
-    <link rel="stylesheet" type="text/css" href="./style.css">
-    <script src="https://kit.fontawesome.com/7b55828ab8.js" crossorigin="anonymous"></script>
+     <script src="https://kit.fontawesome.com/7b55828ab8.js" crossorigin="anonymous"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap');
     </style>
     <style>
+        img {
+            width: 100%;
+        }
+
         div.image-logo-1 {
             display: flex;
             justify-content: center;
+            width: 5%;
+            margin: auto;
         }
-        
+
         div.image-product {
             margin: 5vh;
-            width: 50%;
+            width: 30%;
         }
-        
-        div.image-product img {
-            width: 80%;
+
+        div.image-product {
+            width: 30%;
+            margin: auto;
         }
-        
+
         div.div-wrapper {
             margin: 5vh;
             display: flex;
             justify-content: right;
         }
-        
+
         div.header-name {
             margin-top: 11vh;
             font-size: 2vh;
             width: 50%;
+            margin: auto 0;
+        }
+
+        @media screen and (max-width: 768px) {
+            div.image-logo-1 {
+                width: 15%;
+            }
+
+            div.image-product {
+                width: 50%;
+                margin: auto;
+            }
         }
     </style>
 </head>
 
 <body>
     <div class="image-logo-1">
-        <img src="../images/logo.jpg" class="logo-1" />
+        <img src="../images/logo.avif" class="logo-1" />
     </div>
     <hr />
     <div class="div-wrapper">
 
         <div class="image-product">
-            <img src="../images/section3.jpg" class="product" />
+            <img src="../images/product.jpg" class="product" />
         </div>
 
         <div class="header-name">
-            <p class="name-product">DERMA E Advanced Peptides and Collagen Moisturizer</p>
+            <p class="name-product">Veet® Gel Cream Hair Remover</p>
             <p class="name-product-text">Thank you for your order, it has been successfully received!</p>
             <p class="name-product-text-bold">Order details: </p>
             <p class="name-product-text"><i class="far fa-check-circle"></i> Order Number: #524956</p>
-            <p class="name-product-text"><i class="far fa-check-circle"></i> Selected payment method: Cash on delivery</p>
+            <p class="name-product-text"><i class="far fa-check-circle"></i> Selected payment method: Cash on delivery
+            </p>
             <p class="name-product-text"><i class="far fa-check-circle"></i> Delivery ETA 3 to 5 business days</p>
         </div>
     </div>
